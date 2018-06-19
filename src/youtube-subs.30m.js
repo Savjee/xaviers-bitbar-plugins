1cae84a83a0205042b1bcce1cf417da95c94ebb9
Xavier Decuyper <hi@savjee.be>
2018-06-19 15:36:15 +0200

    Cache YouTube subscribers for when I’m offline

--- src/youtube-subs.30m.js
+++ src/youtube-subs.30m.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env /usr/local/bin/node
 
 const request = require('request-promise-native');
+const fs = require('fs');
 require('dotenv').config({ path: __dirname + '/../.env' });
 
 const reqParams = {
@@ -10,20 +11,51 @@ const reqParams = {
 };
 
 request(reqParams)
-  .then(data => {
+  .then(async (data) => {
     if (data.items && data.items[0] && data.items[0].statistics) {
-      console.log(numberWithCommas(data.items[0].statistics.subscriberCount));
+
+      const subCount = numberWithCommas(data.items[0].statistics.subscriberCount);
+      cacheSubs(subCount);
+      console.log(subCount);
       console.log('---');
+      console.log('Subs: ' + numberWithCommas(data.items[0].statistics.subscriberCount));
       console.log('Views: ' + numberWithCommas(data.items[0].statistics.viewCount));
       console.log('Video\'s: ' + numberWithCommas(data.items[0].statistics.videoCount));
+      console.log('---');
+      console.log('Socialblade | href=https://socialblade.com/youtube/channel/UCnxrdFPXJMeHru_b4Q_vTPQ')
+      console.log('Channel | href=https://www.youtube.com/savjee')
     }
   })
-  .catch(error => {
-      console.log('Error');
+  .catch(async (error) => {
+      console.log(await getCachedSubs() + '!');
       console.log('---');
-      console.log(error);
+      console.log('No internet connection.');
+      console.log('This data was retrieved from cache.');
   });
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+/**
+ * 
+ */
+function cacheSubs(subCount){
+  return new Promise((resolve, reject) => {
+    fs.writeFile('/tmp/youtube-subs.cache', subCount, (err) => {
+      if (err) return reject(err);
+
+      return resolve();
+    });
+  });
+}
+
+function getCachedSubs(){
+    return new Promise((resolve, reject) => {
+      fs.readFile('/tmp/youtube-subs.cache', (err, data) => {
+        if (err) return reject(err);
+
+        return resolve(data);
+      });
+  });
 }
